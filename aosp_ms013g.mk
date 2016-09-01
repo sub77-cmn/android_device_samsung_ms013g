48a7d435f2c1b74f08e774ab3775db59780390f7
Aditya Upreti <aupreti72@gmail.com>
2016-09-01 10:04:36 +0530

    Cm.mk --> aosp.mk

--- aosp_ms013g.mk
+++ aosp_ms013g.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2016 The Android OpenSource Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit some common Bliss stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
@@ -21,8 +21,7 @@ $(call inherit-product, device/samsung/ms013g/full_ms013g.mk)
 # Release name
 PRODUCT_RELEASE_NAME := SM-G7102
 PRODUCT_DEVICE := ms013g
-PRODUCT_NAME := cm_ms013g
+PRODUCT_NAME := aosp_ms013g
 
 # Product property
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ms013gxx TARGET_DEVICE=ms013g BUILD_FINGERPRINT="samsung/ms013gxx/ms013g:4.4.2/KOT49H/G7102XXUBOB1:user/release-keys" PRIVATE_BUILD_DESC="ms013gxx-user 4.4.2 KOT49H G7102XXUBOB1 release-keys"
-
