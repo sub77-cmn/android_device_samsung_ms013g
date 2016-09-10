9b2ce1fafa20bd1f0c6efd9e9484f9c54e109cdb
sub77 <sub77@ymail.com>
2016-09-11 01:17:18 +0200

    ms013g: Update kernel config
    Signed-off-by: sub77 <sub77@ymail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,15 +32,16 @@ TARGET_GLOBAL_CPPFLAGS += -DANDROID_MULTI_SIM
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom vmalloc=400M user_debug=23 msm_rtb.filter=0x37
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 TARGET_KERNEL_SOURCE := kernel/samsung/ms013g
-TARGET_KERNEL_CONFIG := cyanogenmod_ms013g_defconfig
+TARGET_KERNEL_CONFIG := cm_msm8226_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := cm_ms013g_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
